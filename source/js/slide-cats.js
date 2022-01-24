ded4a82dd01082b17608e5ad3f55569b2a584f48
WilhelmYakunin <v.jakunin@inbox.ru>
2022-01-24 21:06:13 +0300

    Декоративные элементы внутренних страниц

--- source/js/slide-cats.js
+++ source/js/slide-cats.js
@@ -2,10 +2,40 @@ const rangeInput = document.getElementById('sliderInput');
 const fatCatImg = document.getElementById('fatCat');
 const catBefore = document.getElementById('catBefore');
 const catAfter = document.getElementById('catAfter');
+const inputTrack = document.getElementsByClassName('example__scrollbar--input')[0];
 
 const slideCats = () => {
+
   const sliderPos = rangeInput.value;
   fatCatImg.style.width = `${sliderPos}%`;
+
+  const getGradient = () => {
+    if (sliderPos >= 50) {
+
+      const green = (100 - sliderPos);
+
+      return `linear-gradient(
+        to left,
+        #ffffff 0%,
+        #ffffff ${100 - sliderPos}%,
+        #68b738 ${green}%,
+        #68b738 100%
+        )`;
+    } else if (sliderPos < 50) {
+
+      const white = (100 - sliderPos);
+
+      return `linear-gradient(
+        to left,
+        #ffffff 0%,
+        #ffffff ${white}%,
+        #68b738 ${100 - sliderPos}%,
+        #68b738 100%
+        )`;
+    } return ``
+  }
+
+  inputTrack.style.setProperty("--color", getGradient());
 }
 
 rangeInput.onchange = slideCats;
@@ -13,9 +43,23 @@ rangeInput.onchange = slideCats;
 catBefore.onclick = () => {
   rangeInput.value = 0;
   fatCatImg.style.width = '0%';
+  inputTrack.style.setProperty("--color", `linear-gradient(
+    to left,
+    #ffffff 0%,
+    #ffffff 100%,
+    #68b738 0%,
+    #68b738 0%
+    )`);
 }
 
 catAfter.onclick = () => {
   rangeInput.value = 100;
   fatCatImg.style.width = '100%';
+  inputTrack.style.setProperty("--color", `linear-gradient(
+    to left,
+    #ffffff 0%,
+    #ffffff 0%,
+    #68b738 0%,
+    #68b738 100%
+    )`);
 }
